f1a397ff746d677508228e485fb122e35d3830b4
Adam Alm <ahalm@studntnu.no>
2025-04-29 16:02:46 +0200

    La til noen kommentarer

--- proj_environment/src/droughtEKTE.py
+++ proj_environment/src/droughtEKTE.py
@@ -6,42 +6,51 @@ import os
 
 url = "https://www.kaggle.com/api/v1/datasets/download/cdminix/us-drought-meteorological-data"
 
+#Funksjon for å laste ned zip-filen fra URL
 def download_zip(url):
     response = requests.get(url)
     return response
 
+#Funksjon for å åpne zip-filen og vise innholdet
 def open_zip(response):
     zip_file = zipfile.ZipFile(io.BytesIO(response.content))
     print("Filer i ZIP:", zip_file.namelist())
     return zip_file
 
+#Funksjon for å hente CSV-filen fra zip-filen
 def get_csv_filename(zip_file):
     return zip_file.namelist()[2]  
 
+#Funksjon for å lese CSV-filen fra zip-filen
 def read_csv_from_zip(zip_file, csv_filename):
     with zip_file.open(csv_filename) as csv_file:
         df = pd.read_csv(csv_file)  
     return df
 
+#Funksjon for å skrive ut noen unike FIPS-koder, som man kan velge fra
 def print_unique_fips(df):
     unique_fips = df['fips'].unique()
     return f"Unike FIPS koder: {unique_fips[:20]}"
 
+#Funksjon for å filtrere data basert på valgte FIPS koder
 def filter_data(df, selected_fips):
     filtered_df = df[df['fips'].isin(selected_fips)]
     return filtered_df
 
+#Funksjon for å lagre filtrert data til CSV-fil i mappen data
 def save_filtered_data(filtered_df):
     filtered_df.to_csv('../data/filtered1_data.csv', index=False)
     print(f"Første rader i filtrert datasett: {filtered_df.head()}")
 
+#Funksjon for å lagre temperaturdata til CSV-fil i mappen data (dette skal brukes til visualisering)
 def save_temperature_data(filtered_df):
-    temp_df = filtered_df[['fips', 'date', 'T2M']]
+    temp_df = filtered_df[['fips', 'date', 'T2M']] #Velger nødvenige kolonner
     temp_df.to_csv('../data/filtered_temperature_data.csv', index=False)
     print(f"Første rader i filtrert datasett: {temp_df.head()}")
 
+#Funksjon for å lagre nedbørsdata til CSV-fil i mappen data (dette skal brukes til visualisering)
 def save_precipitation_data(filtered_df):
-    prec_df = filtered_df[['fips', 'date', 'PRECTOT']]
+    prec_df = filtered_df[['fips', 'date', 'PRECTOT']] #Velger nødvendige kolonner
     prec_df.to_csv('../data/filtered_precipitation_data.csv', index=False)
     print(f"Første rader i filtrert datasett: {prec_df.head()}")
 
@@ -53,20 +62,23 @@ def new_file():
 
             if new.lower() == "y":
 
-                print("Creating new file")
+                print("Lager ny fil...")
+
+                #Kjører tidligere definerte funksjoner for å danne ny fil (vil erstatte eksisterende fil hvis filnavnet er likt)
+
                 response = download_zip(url)
                 zip_file = open_zip(response)
                 csv_filename = get_csv_filename(zip_file)
                 df = read_csv_from_zip(zip_file, csv_filename)
                 print_unique_fips(df)
-                selected_fips = [1001, 1003, 1005]
+                selected_fips = [1001, 1003, 1005] #Eksempel på FIPS koder, kan endres
                 filtered_df = filter_data(df, selected_fips)
                 
                 save_filtered_data(filtered_df)
                 save_temperature_data(df)
                 save_precipitation_data(df)
 
-                print(f"Antall rader etter filtrering: {len(filtered_df)}")
+                print(f"Antall rader etter filtrering: {len(filtered_df)}") #For å få en oversikt over størrelse på fil, og hva man kan gjøre med den
 
                 break
 
--- proj_environment/src/drought_stat_analysis.py
+++ proj_environment/src/drought_stat_analysis.py
@@ -5,31 +5,31 @@ from importlib import reload
 import matplotlib.pyplot as plt
 import numpy as np
 
-reload(dc)
+reload(dc) #Importerer modul, bruker reload() for å være sikker på at all kode er oppdatert
 
-fips_codes = dc.fips_codes
+fips_codes = dc.fips_codes #Henter valgte FIPS koder
 df = pd.read_csv("../data/filtered1_data_cleaned.csv")
-ignored_cols = ["fips", "date", "score"]
+ignored_cols = ["fips", "date", "score"] #Disse kolonnene skal ikke brukes i korrelasjonsanalysen (ikke målte verdier)
 
 def corr_analysis(df, fips_codes, var_1, var_2):
 
     try:
         
-        corr_values = []
+        corr_values = [] #Lager en tom liste for å lagre korrelasjonsverdier
 
-        for fips in fips_codes:
+        for fips in fips_codes: #Itererer gjennom hver FIPS kode fra ønskt liste med FIPS koder
 
-            analyze = df[df["fips"] == fips][[var_1, var_2]].dropna()
+            analyze = df[df["fips"] == fips][[var_1, var_2]].dropna() #Filtrerer data for hver FIPS kode og fjerner eventuelle NaN verdier
 
-            if len(analyze) < 2:
+            if len(analyze) < 2: #Må ha minst 2 datapunkter for at korrelasjonsanalysen skal gi mening
 
                 print(f"Ikke nok data for FIPS {fips}.")
 
-                continue
+                continue #Fortsetter til neste FIPS kode
 
-            correlation, p_value = pearsonr(analyze[var_1], analyze[var_2])
+            correlation, p_value = pearsonr(analyze[var_1], analyze[var_2]) #Utfører korrelasjonsanalyse med pearsonr() funksjonen fra scipy.stats
 
-            if abs(correlation) < 0.3:
+            if abs(correlation) < 0.3: #Et valgt intervall for å definere styrke på korrelasjonen
 
                 strength = "Svak korrelasjon"
 
@@ -41,22 +41,24 @@ def corr_analysis(df, fips_codes, var_1, var_2):
 
                 strength = "Sterk korrelasjon"
 
-            significance = "Signifikant" if p_value < 0.05 else "Ikke signifikant"
+            significance = "Signifikant" if p_value < 0.05 else "Ikke signifikant" #P-verdier under 0.05 er typisk signifikante
+
+            #Skriver ut diverse verdier for hver FIPS kode
 
             print(f"FIPS kode: {fips}")
             print(f"Korrelasjonsverdi: {correlation:.2f}")
             print(f"P-verdi: {p_value:.10f}")
             print(f"Resultat: {strength} | {significance}")
 
-            corr_values.append(round(float(correlation), 2))
+            corr_values.append(round(float(correlation), 2)) #Legger til korrelasjonsverdi i listen fra tidligere
 
         return corr_values
     
     except KeyError as e:
 
-        return f"KeyError: {e}. Vennligst sjekk variabelnavn"
+        return f"KeyError: {e}. Vennligst sjekk variabelnavn" #Håndterer KeyError hvis variabelnavnene ikke finnes i datasettet
 
-def color_corr(corr_value):
+def color_corr(corr_value): #EN funksjon for definere farge basert på korrelasjonsverdier
     
     if corr_value <= -0.6:
 
@@ -78,66 +80,68 @@ def color_corr(corr_value):
 
         return "blå"
 
-def visualize_corr(fips_codes, corr_values, var_1, var_2):
+def visualize_corr(fips_codes, corr_values, var_1, var_2): #Funksjon for å visualisere korrelasjonsverdier med matplotlib
 
     try:
 
         colors = [color_corr(corr) for corr in corr_values]
 
-        plt.figure(figsize=(12, 8))
-        bars = plt.bar(fips_codes, corr_values, color=colors)
-        plt.title(f"Korrelasjon mellom {var_1} og {var_2} for hver FIPS kode", fontsize=16)
-        plt.xlabel("FIPS koder", fontsize=14)
-        plt.ylabel("Korrelasjonsverdi", fontsize=14)
-        plt.axhline(0, color='grønn', linewidth=0.8, linestyle="--")
+        plt.figure(figsize=(12, 8)) #Seter figurstørrelse
+        bars = plt.bar(fips_codes, corr_values, color=colors) #ager stolpediagram for hver FIPS kode med korrelasjonsverdier og farger
+        plt.title(f"Korrelasjon mellom {var_1} og {var_2} for hver FIPS kode", fontsize=16) #Tittel
+        plt.xlabel("FIPS koder", fontsize=14) #Tittel på x-aksen, og størrelse
+        plt.ylabel("Korrelasjonsverdi", fontsize=14) #Tittel på y-aksen, og størrelse
+        plt.axhline(0, color='grønn', linewidth=0.8, linestyle="--") #Legger til horisontal linje på 0 for å skille mellom positive og negative korrelasjonsverdier
 
-        for bar, r in zip(bars, corr_values):
+        for bar, r in zip(bars, corr_values): #Itererer gjennom hver stolpe og korrelasjonsverdi for å legge til tekst på toppen av hver stolpe
 
-            plt.text(bar.get_x() + bar.get_width() / 2, r + 0.01, f"{r:.2f}", ha="center", va="bottom", fontsize=12)
+            plt.text(bar.get_x() + bar.get_width() / 2, r + 0.01, f"{r:.2f}", ha="center", va="bottom", fontsize=12) #Skriver tekst på toppen av hver stolpe
+
+        #Størrelse på aksene
 
         plt.xticks(fontsize=12)
         plt.yticks(fontsize=12)
 
-        plt.tight_layout()
-        plt.show()
+        plt.tight_layout() #For å unngå overlapping av elementer i figuren
+        plt.show() #Visualiserer for brukeren
     
     except KeyError as e:
 
-        return f"KeyError: {e}. Vennligst sjekk variabelnavn"
+        return f"KeyError: {e}. Vennligst sjekk variabelnavn" #Håndterer KeyError hvis variabelnavnene ikke finnes i datasettet
     
-def corr_var1_and_rest(df, fips_codes, var, exclude=ignored_cols):
+def corr_var1_and_rest(df, fips_codes, var, exclude=ignored_cols): #Funksjon for å lage korrelasjonsmatrise for en valgt variabel og resten av datasettet
 
     try:
 
-        all_corr = {}
-        usable_cols = df.columns.difference(exclude + [var])
+        all_corr = {} #Lager et tomt dictionary for å lagre korrelasjonsverdier for hver FIPS kode
+        usable_cols = df.columns.difference(exclude + [var]) #Liste med kbrukbare kolonner i analysen (alle bortsett fra de som er i exclude og variabel vi ønsker å analysere)
 
-        for fips in fips_codes:
+        for fips in fips_codes: #Itererer gjennom hver FIPS kode
 
-            next_set = df[df["fips"] == fips]
-            new_corr_values = {}
+            next_set = df[df["fips"] == fips] #Filtrerer datasettet for hver FIPS kode
+            new_corr_values = {} #Lager et tomt dictionary for å lagre korrelasjonsverdier for hver kolonne i datasettet
 
-            for col in usable_cols:
+            for col in usable_cols: #Itererer gjennom hver (brukbar) kolonne i datasettet
 
-                new_data = next_set[[var, col]].dropna()
+                new_data = next_set[[var, col]].dropna() 
 
-                if len(new_data) > 1:
+                if len(new_data) > 1: 
                 
-                    corr_coeff, prsn_no_use = pearsonr(new_data[var], new_data[col])
+                    corr_coeff, prsn_no_use = pearsonr(new_data[var], new_data[col]) #prsn_no_use er p-verdien fra pearsonr() funksjonen (trengs ikke her)
                     new_corr_values[col] = round(float(corr_coeff), 2)
 
                 else:
 
-                    new_corr_values[col] = None
+                    new_corr_values[col] = None #Ikke nok data for å beregne korrelasjonsverdi
         
-            all_corr[fips] = new_corr_values
+            all_corr[fips] = new_corr_values #Lagrer korrelasjonsverdier for hver FIPS kode i dictionary
     
-        return all_corr
+        return all_corr 
     
     except KeyError as e:
-        return f"KeyError: {e}. Vennligst sjekk variabelnavn"
+        return f"KeyError: {e}. Vennligst sjekk variabelnavn" #Håndterer KeyError hvis variabelnavnene ikke finnes i datasettet
 
-def visualize_corr_var1_and_rest(corr_dict, fips_codes, var):
+def visualize_corr_var1_and_rest(corr_dict, fips_codes, var): #Funksjon for å visualisere korrelasjonsmatrise for en valgt variabel og resten av datasettet, her med spider chart
     
     while True:
 
@@ -147,7 +151,7 @@ def visualize_corr_var1_and_rest(corr_dict, fips_codes, var):
             
             try:
 
-                what_fips = int(input("Skriv FIPS kode for å visualisere korrelasjon med andre andre variabler: "))
+                what_fips = int(input("Skriv FIPS kode for å visualisere korrelasjon med andre andre variabler: ")) #Kan kun visualisere for en FIPS kode om gangen
             
             except ValueError:
 
@@ -160,32 +164,34 @@ def visualize_corr_var1_and_rest(corr_dict, fips_codes, var):
 
             else:
 
-                next_data = corr_dict.get(what_fips)
+                next_data = corr_dict.get(what_fips) #Henter korrelasjonsverdier for valgt FIPS kode
 
-            if not next_data:
+            if not next_data: #Hvis vi ikke finner data for valgt FIPS kode
 
                 return f"Ingen data tilgjengelig for FIPS koden {what_fips}."
             
-            new_vars = list(next_data.keys())
-            new_corrs = [c if c is not None else 0 for c in next_data.values()]
+            new_vars = list(next_data.keys()) 
+            new_corrs = [c if c is not None else 0 for c in next_data.values()] #Setter korrelasjonsverdier til 0 hvis det ikke er nok data for å beregne korrelasjonsverdi
+            
+            #Legger til første variabel/korrelasjonsverdi på slutten av listen for å lage en lukket sirkel i spider chart
 
             new_vars.append(new_vars[0])
             new_corrs.append(new_corrs[0])
 
             angle_radar = np.linspace(0, 2 * np.pi, len(new_corrs), endpoint=False).tolist()
 
-            plt.figure(figsize=(8, 8))
-            ax = plt.subplot(111, polar=True)
+            plt.figure(figsize=(8, 8)) #Figurstørrelse
+            ax = plt.subplot(111, polar=True) #Setter subplot til polar for spider chart
 
-            ax.plot(angle_radar, new_corrs, color="cyan", linewidth=1.5, linestyle="solid")
-            ax.fill(angle_radar, new_corrs, color="cyan", alpha=0.25)
+            ax.plot(angle_radar, new_corrs, color="cyan", linewidth=1.5, linestyle="solid") #Plotter linjen for korrelasjonsverdier
+            ax.fill(angle_radar, new_corrs, color="cyan", alpha=0.25) #Fyller området under linjen med farge
 
-            ax.set_xticks(angle_radar[:-1])
+            ax.set_xticks(angle_radar[:-1]) #Setter x-aksen til vinklene for hver variabel
             ax.set_xticklabels(new_vars[:-1], fontsize=11)
             ax.set_yticks([-1, -0.5, 0, 0.5, 1])
             ax.set_yticklabels([-1, -0.5, 0, 0.5, 1], fontsize=10)
-            ax.set_title(f"Korrelasjon mellom {var} og resten av variablene i FIPS {what_fips}", fontsize=15)
-            ax.grid(True)
+            ax.set_title(f"Korrelasjon mellom {var} og resten av variablene i FIPS {what_fips}", fontsize=15) #Tittel på spider chartet
+            ax.grid(True) #Legger til rutenett for bedre lesbarhet
 
             plt.tight_layout()
             plt.show()
